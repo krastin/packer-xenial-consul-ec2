71e3fbd74bffb2d4e826c1981fe9dcf987e14d32
Krastin Krastev <krastin@hashicorp.com>
2019-09-12 18:45:12 +0300

    cleanup

--- scripts/configure_consul.sh
+++ scripts/configure_consul.sh
@@ -4,6 +4,5 @@
 echo "{ \"retry_join\": $RETRYIPS }" > /etc/consul.d/retry_join.json
 
 # Set up server settings
-echo "" > /etc/consul.d/server.json
 echo "{ \"server\": $SERVER }" > /etc/consul.d/server.json
 echo "{ \"bootstrap_expect\": $BOOTSTRAP }" >> /etc/consul.d/server.json
