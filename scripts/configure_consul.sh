6659f6fc18247c9bff35dc83b0d5a2676242237c
Krastin Krastev <krastin@hashicorp.com>
2019-09-13 13:02:46 +0300

    fix json file from configurator

--- scripts/configure_consul.sh
+++ scripts/configure_consul.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
 
 # Populate other node's IPs to retry_join
+cat <<EOF >/etc/consul.d/retry_join.json
+{
+  "retry_join": $RETRYIPS
+}
+EOF
 echo "{ \"retry_join\": $RETRYIPS }" > /etc/consul.d/retry_join.json
 
+if [ "$SERVER" == "true" ]; then
+    # setup server settings
+    cat <<EOF >/etc/consul.d/server.json
+{
+  "server": true,
+  "bootstrap_expect": $BOOTSTRAP
+}
+EOF
+else
+    # setup client settings
+    cat <<EOF >/etc/consul.d/client.json
+{
+  "server": false
+}
+EOF
+fi
 # Set up server settings
-echo "{ \"server\": $SERVER }" > /etc/consul.d/server.json
-echo "{ \"bootstrap_expect\": $BOOTSTRAP }" >> /etc/consul.d/server.json
