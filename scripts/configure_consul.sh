74cf2c95985a747fe19fab5a77f7ac6b084dbb72
Krastin Krastev <krastin@hashicorp.com>
2020-07-03 12:20:55 +0200

    standardize configure_consul.sh across consul/vault boxes

--- scripts/configure_consul.sh
+++ scripts/configure_consul.sh
@@ -1,26 +1,66 @@
 #!/usr/bin/env bash
 
-# Populate other node's IPs to retry_join
-cat <<EOF >/etc/consul.d/retry_join.json
+## Variables
+# $NODE_NAME
+# $RETRYIPS
+# $SERVER
+# $ACCESS_KEY_ID
+# $SECRET_ACCESS_KEY
+# $CLUSTER
+
+# Set up basic consul settings
+if [ ! -z "$NODE_NAME" ]
+then
+  NODE_NAME=$(cat /etc/hostname)
+fi
+sudo cat <<EOF >/etc/consul.d/basic_config.json
 {
-  "retry_join": $RETRYIPS
+  "node_name": "${NODE_NAME}",
+  "data_dir": "/opt/consul",
+  "log_level": "DEBUG",
+  "enable_debug": true
 }
 EOF
 
 if [ "$SERVER" == "true" ]; then
-    # setup server settings
-    cat <<EOF >/etc/consul.d/server.json
+  # setup server settings
+  sudo cat <<EOF >/etc/consul.d/server.json
 {
   "server": true,
-  "bootstrap_expect": $BOOTSTRAP
+  "bootstrap_expect": ${BOOTSTRAP}
 }
 EOF
 else
-    # setup client settings
-    cat <<EOF >/etc/consul.d/client.json
+  # setup client settings
+  sudo cat <<EOF >/etc/consul.d/client.json
 {
   "server": false
 }
 EOF
 fi
-# Set up server settings
+
+# Populate other node's IPs to retry_join
+if [ ! -z "$RETRYIPS"]; then
+  sudo cat <<EOF >/etc/consul.d/retry_join.json
+{
+  "retry_join": ${RETRYIPS}
+}
+EOF
+fi
+
+# Use cloud join to find other nodes
+if [ ! -z "$ACCESS_KEY_ID" ] && [ ! -z "$SECRET_ACCESS_KEY" ] && [ ! -z "$CLUSTER" ]
+then
+  sudo cat <<EOF >/etc/consul.d/cloud_join.hcl
+retry_join = ["provider=aws tag_key=CLUSTER tag_value=${CLUSTER} access_key_id=${ACCESS_KEY_ID} secret_access_key=${SECRET_ACCESS_KEY}"]    
+EOF
+fi
+
+# make sure everything is writeable by consul
+sudo chown -R consul /etc/consul.d
+
+# enable and start consul
+sudo systemctl enable consul
+sudo systemctl start consul
+
+sleep 3s
